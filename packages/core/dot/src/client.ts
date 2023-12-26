7e792734ea70b3f142c9874048fc3d6f466bc90d
x71c9 <108585118+x71c9@users.noreply.github.com>
2023-12-26 11:27:57 +0100

    before testing dot directory.

---
+++ packages/core/dot/src/client.ts
@@ -0,0 +1,42 @@
+/**
+ *
+ * Client module
+ *
+ * @packageDocumentation
+ *
+ */
+
+import mongodb, {MongoClient, ServerApiVersion} from 'mongodb';
+import {log} from './log/index';
+
+export type ClientParams = {
+  uri: string;
+  db_name: string;
+};
+
+export class Client{
+  protected client: mongodb.MongoClient;
+  protected db: mongodb.Db;
+  constructor(params: ClientParams) {
+    this.client = new MongoClient(params.uri, {
+      serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+      },
+    });
+    this.db = this.client.db(params.db_name);
+  }
+  public async connect() {
+    log.trace('Connecting...');
+    await this.client.connect();
+    log.trace('Connected.');
+  }
+  public async disconnect() {
+    log.trace('Disconnecting...');
+    await this.client.close();
+    log.trace('Disconnected.');
+  }
+}
+
+
---
+++ packages/core/dot/src/log/index.ts
@@ -0,0 +1,10 @@
+/**
+ *
+ * Log index module
+ *
+ * @packageDocumentation
+ *
+ */
+
+import ion from 'i0n';
+export const log = ion.create();
