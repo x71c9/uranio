55f67b3bb653737b4aaf825449da782033842ce2
x71c9 <108585118+x71c9@users.noreply.github.com>
2023-12-29 13:09:01 +0100

    updated readme, .gitignore, script version.

--- scripts/version.sh
+++ scripts/version.sh
@@ -1,6 +1,33 @@
 #!/bin/bash
 
-SEMANTIC_NAME=$1
+if [[ `git status --porcelain` ]]; then
+  echo
+  echo "Error: Git working directory is not clean. Please commit your changes or stash them."
+  echo
+  exit 1
+fi;
+
+SEMANTIC_NAME=""
+FORCE_FLAG=""
+
+# Parse command line arguments
+while [ "$#" -gt 0 ]; do
+  case "$1" in
+    --force)
+      FORCE_FLAG="--force"
+      shift
+      ;;
+    *)
+      if [ -z "$SEMANTIC_NAME" ]; then
+        SEMANTIC_NAME="$1"
+      else
+        echo "Unknown argument: $1"
+        exit 1
+      fi
+      shift
+      ;;
+  esac
+done
 
 if [ "$SEMANTIC_NAME" == "" ]; then
   echo
@@ -10,43 +37,53 @@ if [ "$SEMANTIC_NAME" == "" ]; then
   exit 1
 fi
 
-case "$SEMANTIC_NAME" in
-  patch)
-    yarn version --patch --no-git-tag-version
-    break;
+if [ "$FORCE_FLAG" == "--force" ]; then
+  response="y"
+else
+  case "$SEMANTIC_NAME" in
+    patch)
+      read -p "Are you sure you want to increase the patch version? [y/n] " response
+      ;;
+    minor)
+      read -p "Are you sure you want to increase the minor version? [y/n] " respnose
+      ;;
+    major)
+      read -p "Are you sure you want to increase the major version? [y/n] " respnose
+      ;;
+    *)
+      echo
+      echo "Invalid semantic name parameter. Valid values are [patch, minor, major]"
+      echo
+      echo "Example: sh $0 patch"
+      exit 1
+      ;;
+  esac
+fi
+
+case "$response" in
+  [Yy]* )
+    case "$SEMANTIC_NAME" in
+      patch)
+        npm version patch
+        ;;
+      minor)
+        npm version minor
+        ;;
+      major)
+        npm version major
+        ;;
+    esac
     ;;
-  minor)
-    while true; do
-      read -p "Are you sure you want to increase the minor version? [y/n] " yn
-      case $yn in
-        [Yy]* ) break;;
-        [Nn]* ) exit;;
-        * ) echo "Please answer [y]es or [n]o.";;
-      esac
-    done
-    yarn version --minor --no-git-tag-version
-    break;
-    ;;
-  major)
-    while true; do
-      read -p "Are you sure you want to increase the major version? [y/n] " yn
-      case $yn in
-        [Yy]* ) break;;
-        [Nn]* ) exit;;
-        * ) echo "Please answer [y]es or [n]o.";;
-      esac
-    done
-    yarn version --major --no-git-tag-version
-    break;
+  [Nn]* ) 
+    echo "Changing version canceled."
+    exit 1
     ;;
   *)
-    echo
-    echo "Invalid semantic name parameter. Valid values are [patch, minor, major]"
-    echo
-    echo "Example: sh $0 patch"
+    echo "Please answer [y]es or [n]o."
     exit 1
     ;;
 esac
 
-VERSION=$(node -p "require('./package.json').version")
-yarn publish --new-version $VERSION --no-git-tag-version
+git push origin
+git push origin v$(node -p "require('./package.json').version")
+
