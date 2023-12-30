2557d8ae228b23b01c699f7feb9e300b5d044448
x71c9 <108585118+x71c9@users.noreply.github.com>
2023-12-30 12:11:56 +0100

    back to _id in atom.

--- .uranio/src/types.ts
+++ .uranio/src/types.ts
@@ -9,15 +9,17 @@
 export * from './query';
 
 export interface atom {
-  _id: primary<string>
+  _id: string
 }
 
-export type primary<T> = T & {__uranio: 'primary'};
+// export type primary<T> = T & {__uranio: 'primary'};
 
 export type unique<T> = T & {__uranio: 'unique'};
 
-type PrimaryAttribute<A extends atom> = {
-  [K in keyof A]: A[K] extends {__uranio: 'primary'} ? K : never;
-}[keyof A];
+// type PrimaryAttribute<A extends atom> = {
+//   [K in keyof A]: A[K] extends {__uranio: 'primary'} ? K : never;
+// }[keyof A];
 
-export type Shape<A extends atom> = Omit<A, PrimaryAttribute<A>>;
+// export type Shape<A extends atom> = Omit<A, PrimaryAttribute<A>>;
+
+export type Shape<A extends atom> = Omit<A, '_id'>;
