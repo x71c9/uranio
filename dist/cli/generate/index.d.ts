ba4ca8e02896962174e5b19de340d78b680ad75b
x71c9 <108585118+x71c9@users.noreply.github.com>
2024-01-17 11:48:29 +0100

    transpiled.

--- dist/cli/generate/index.d.ts
+++ dist/cli/generate/index.d.ts
@@ -2,6 +2,16 @@
  *
  * Generate index module
  *
+ * `generate` command do the following:
+ *
+ * - copy the directory `.uranio` from its root directory into the
+ *   `node_modules` of the project where the command is run.
+ *
+ * - After parsing the types with `plutonio` it updates the copied typescript
+ *   files.
+ *
+ * - It then build the updated `.uranio` files as a typescript project.
+ *
  * @packageDocumentation
  */
 import * as t from '../types';
