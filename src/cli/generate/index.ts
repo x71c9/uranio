ba7c6f79ee2a5533d8528a965d886aa80240e8ee
x71c9 <108585118+x71c9@users.noreply.github.com>
2024-01-26 19:49:22 +0100

    removed primary from generate command.

--- src/cli/generate/index.ts
+++ src/cli/generate/index.ts
@@ -207,8 +207,8 @@ function _generate_mongodb_uranio_types_module_text() {
   text += ` *\n`;
   text += ` */\n`;
   text += `\n`;
-  text += `import {mongodb_atom as atom, primary, mongodb_id} from './atom';\n`;
-  text += `export {atom, primary, mongodb_id};\n`;
+  text += `import {mongodb_atom as atom, mongodb_id} from './atom';\n`;
+  text += `export {atom, mongodb_id};\n`;
   text += `\n`;
   return text;
 }
@@ -223,8 +223,8 @@ function _generate_mysql_uranio_types_module_text() {
   text += ` *\n`;
   text += ` */\n`;
   text += `\n`;
-  text += `import {mysql_atom as atom, primary} from './atom';\n`;
-  text += `export {atom, primary};\n`;
+  text += `import {mysql_atom as atom} from './atom';\n`;
+  text += `export {atom};\n`;
   text += `\n`;
   return text;
 }
