cf95f46cda624123dfee8b6ee0d57b27e4b0ccd4
x71c9 <108585118+x71c9@users.noreply.github.com>
2024-10-17 11:29:34 +0200

    feat: deep_clone with structuredClone

--- src/cli/utils/object.ts
+++ src/cli/utils/object.ts
@@ -20,5 +20,6 @@ export function deep_clone<T>(obj: T): T {
   if (!obj || typeof obj !== 'object') {
     return obj;
   }
-  return JSON.parse(JSON.stringify(obj));
+  // return JSON.parse(JSON.stringify(obj));
+  return structuredClone(obj);
 }
