a89ebd2732d3c044e054a836246d55bd4fbdcf5d
x71c9 <108585118+x71c9@users.noreply.github.com>
2025-07-20 19:46:47 +0200

    feat: add poolsize. check if db is connected. update README.

--- .uranio/src/client/mongodb.ts
+++ .uranio/src/client/mongodb.ts
@@ -27,15 +27,26 @@ export class MongoDBClient {
         version: ServerApiVersion.v1,
         strict: true,
         deprecationErrors: true,
+        maxPoolSize: 10,
+        minPoolSize: 0,
       },
     } as MongoClientOptions;
     this.client = new MongoClient(params.uri, options);
     this.db = this.client.db(params.db_name, {ignoreUndefined: true});
   }
   public async connect() {
-    log.trace('Connecting...');
-    await this.client.connect();
-    log.trace('Connected.');
+    try {
+      // Check if the client is already connected by pinging the database
+      await this.client.db().command({ping: 1});
+      log.trace('MongoDB is already connected.');
+    } catch (error) {
+      log.trace('Connecting to MongoDB...');
+      await this.client.connect();
+      log.trace('MongoDB connected.');
+    }
+    // log.trace('Connecting...');
+    // await this.client.connect();
+    // log.trace('Connected.');
   }
   public async disconnect() {
     log.trace('Disconnecting...');
