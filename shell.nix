d77b1a5cebdb5d3f18c4ac4a02a188e4fa3e20d8
x71c9 <108585118+x71c9@users.noreply.github.com>
2026-02-23 12:04:55 +0100

    chore: remove yarn

--- shell.nix
+++ shell.nix
@@ -2,13 +2,12 @@
 pkgs.mkShell {
   packages = [
     pkgs.nodejs_24
-    pkgs.nodePackages_latest.yarn
     pkgs.nodePackages_latest.typescript
     pkgs.nodePackages_latest.typescript-language-server
   ];
   shellHook = ''
     export NODE_ENV=development
-    echo "node: $(node -v) | npm: $(npm -v) | yarn: $(yarn -v) | typescript: $(tsc -v)"
+    echo "node: $(node -v) | npm: $(npm -v) | typescript: $(tsc -v)"
   '';
 }
 
